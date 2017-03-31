4fc093ff12a4c09233d692d2e0668a2fe029466d
Andrew Ghaly <ghalya@wit.edu>
2017-03-31 18:10:11 -0400

    Fixed path fixer/renamed

--- RenameJava.py
+++ RenameJava.py
@@ -1,12 +1,25 @@
 import os
+import re
+import fileinput
 
-path = """/pa6-calendar-student/source"""
+path = """/Users/andrewghaly/Google Drive/Home/School/Classes/Semester VII/Artificial Intelligence/Project/pa6-calendar-student/source"""
 for root, dirs, files in os.walk(path):
     for file in files:
         if file.endswith(".java"):
             originalPath = os.path.join(root, file)
             parentFolder = os.path.basename(os.path.normpath(root))
-            finalPath = os.path.join(path, file[0:4] + parentFolder[-3:] + file[4:])
+            finalPath = os.path.join(path, file[:-5] + parentFolder + file[-5:])
             os.rename(originalPath, finalPath)
-            os.rmdir(root)
 
+            className = file[:-5] + parentFolder
+
+            f = open(finalPath, "r")
+            lines = f.readlines()
+            f.close()
+
+            f = open(finalPath, "w")
+            for line in lines:
+                line = re.sub(r"class %s" % file[:-5], "class " + className, line)
+                f.write(line)
+
+        os.rmdir(root)
