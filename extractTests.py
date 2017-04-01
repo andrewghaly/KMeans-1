cd2ba467e6d03fec3e302829a36c094590c64cdb
alfanoe <alfanoe@wit.edu>
2017-04-01 18:19:03 -0400

    Now runs for each student code .java file that exists in studencode directory Cleaned up output data with re, now just storing test names

--- extractTests.py
+++ extractTests.py
@@ -4,66 +4,61 @@ import os
 import sys
 import subprocess
 import string
+import glob
+import re
 
-# TODO replace alfanoe stuff with vars
-# TODO change hardcoded number of exercises
 # TODO change output format for k-means
 
-#RUN FROM DIR pa6-calendar-student
-NUM_ASSIGNMENTS = 18
+# RUN FROM DIR pa6-calendar-student
 
-javafile = 'src/edu/wit/cs/comp1000/tests/PA6aTestCase.java'
-compile = "javac " + javafile + ' -classpath src:src/junit-4.12.jar'
+utestfile = 'src/edu/wit/cs/comp1000/tests/PA6aTestCase.java'
+studentcode = 'src/calendar/source/'
+compile = "javac " + utestfile + ' -classpath src:src/junit-4.12.jar'
 run = 'java -cp src/junit-4.12.jar:src/hamcrest-core-1.3.jar:src org.junit.runner.JUnitCore edu.wit.cs.comp1000.tests.PA6aTestCase'
+
 
 def checkTests(out):
     if "OK (" in out:
         return "PASSED ALL"
     else:
         print "SOMETHING FAILED !!"
-        fail = []
+        failed = []
+        r = re.compile(r"\) |\(")
         for line in out.split('\n'):
-            #print line
             if ") test" in line:
-                #print "FAILED", line
-                list = line.split("(")
-                #print "LIST", list
-                fail.append(list[0])
-                #print "FAIL", fail
-        return "FAILED", fail
+                # list = line.split("(")
+                list = r.split(line)
+                failed.append(list[1])
+        return failed
 
 
-for i in range(1, NUM_ASSIGNMENTS + 1):
+i = 1
+for file in glob.glob(studentcode + '*.java'):
     try:
-        f = open(javafile, 'r')
+        f = open(utestfile, 'r')
     except IOError, e:
         print "Could not open file."
         sys.exit()
     filedata = f.read()
     f.close()
 
-    # TODO: regex
-    oldtest = "    private static calendar.source.PA6a" + str(i - 1) + " testClass;"
     newtest = "    private static calendar.source.PA6a" + str(i) + " testClass;"
 
-    if i != 1:
-        newdata = filedata.replace(oldtest, newtest)
+    for line in filedata.split('\n'):
+        if "private static calendar.source.PA6a" in line:
+            oldtest = line
+            newdata = filedata.replace(oldtest, newtest)
 
-        f = open(javafile, 'w')
-        f.write(newdata)
-        f.close()
-    else:
-        oldtest = "    private static calendar.source.PA6a18 testClass;"
-        newdata = filedata.replace(oldtest, newtest)
-
-        f = open(javafile, 'w')
-        f.write(newdata)
-        f.close()
+            f = open(utestfile, 'w')
+            f.write(newdata)
+            f.close()
 
     os.system(compile)
-    out = subprocess.Popen([run], stdout=subprocess.PIPE, shell=True) #run java file via subprocess to fix stdout and stderr
+    out = subprocess.Popen([run], stdout=subprocess.PIPE,
+                           shell=True)  # run java file via subprocess to fix stdout and stderr
     out = out.stdout.read()
 
     print "\nCHECKING", i, "\n", checkTests(out)
+    i += 1
 
 print "\nCOMPLETE"
